685e5575214c1dffb97ba19aa166145abe5f36f3
boberro <m.cyraniak@gmail.com>
2022-02-23 13:43:36 +0100

    fix: addon name

--- @rof_sog_addons/mod.cpp
+++ @rof_sog_addons/mod.cpp
@@ -1,7 +1,7 @@
-name = "Republic of Folk Addons";
+name = "Republic of Folk SOG Addons";
 picture = "\x\rof\addons\main\rof.paa";
 hidePicture = "false";
 hideName = "false";
 actionName = "Website";
 action = "https://republicoffolk.org/";
-description = "Republic of Folk Addons";
+description = "Republic of Folk SOG Addons";
